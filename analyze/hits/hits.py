c86053578e56c71118d41d3ef734fe3531f6cdf7
daniel0321forever <daniel.bb0321@gmail.com>
2024-03-11 02:16:33 +0800

    analyze

--- analyze/hits/hits.py
+++ analyze/hits/hits.py
@@ -12,19 +12,22 @@ from ExaTrkXPlotting import Plotter, PlotConfig
 from ExaTrkXPlots import hits, pairs, particles
 
 if __name__ == '__main__':
+    """
+    Notice: we shoud run analyze in analyze dir
+    """
     reader = DataReader(
-        config_path='../configs/reading/hits.yaml',
-        base_dir='../../data'
+        config_path='config/hits.yaml',
+        base_dir='.'
     )
 
-    event_ids = range(455, 458)
+    event_ids = ["0143", "0146", "0184"]
 
     plt.rcParams.update({'font.size': 16})
 
     for event_id in event_ids:
         data = reader.read_one(evtid=event_id)
 
-        save = Path(f'../../output/plots/10k+5k/overview/event{data.evtid}')
+        save = Path(f'output/hits/event{data.evtid}')
         save.mkdir(exist_ok=True, parents=True)
 
         hit_data = data['hits']
@@ -54,50 +57,50 @@ if __name__ == '__main__':
             ]
         }).plot(save=save/'hits.pdf')
 
-        hit_with_particles = pd.merge(
-            hit_data,
-            particle_data[['particle_id', 'particle_type']],
-            on='particle_id'
-        )
-        hit_filtered = hit_with_particles[hit_with_particles['particle_type'].isin([13, -13])]
-        pair_filtered = pair_data[
-            pair_data['hit_id_1'].isin(hit_data['hit_id']) &
-            pair_data['hit_id_2'].isin(hit_data['hit_id'])
-        ]
+        # hit_with_particles = pd.merge(
+        #     hit_data,
+        #     particle_data[['particle_id', 'particle_type']],
+        #     on='particle_id'
+        # )
+        # hit_filtered = hit_with_particles[hit_with_particles['particle_type'].isin([13, -13])]
+        # pair_filtered = pair_data[
+        #     pair_data['hit_id_1'].isin(hit_data['hit_id']) &
+        #     pair_data['hit_id_2'].isin(hit_data['hit_id'])
+        # ]
 
-        fig, ax = plt.subplots(figsize=(8, 8), tight_layout=True)
+        # fig, ax = plt.subplots(figsize=(8, 8), tight_layout=True)
 
-        Plotter(fig, {
-            ax: [
-                PlotConfig(
-                    plot=hits.hit_plot,
-                    data={
-                        'hits': hit_data
-                    }
-                ),
-                PlotConfig(
-                    plot=particles.particle_track_with_production_vertex,
-                    data={
-                        'hits': hit_filtered,
-                        'pairs': pair_filtered,
-                        'particles': particle_data,
-                        'truth': data['truth']
-                    },
-                    args={
-                        'line_width': 1.0
-                    }
-                ),
-                PlotConfig(
-                    plot=particles.particle_types,
-                    data={
-                        'hits': hit_filtered,
-                        'pairs': pair_filtered,
-                        'particles': particle_data,
-                        'truth': data['truth']
-                    }
-                )
+        # Plotter(fig, {
+        #     ax: [
+        #         PlotConfig(
+        #             plot=hits.hit_plot,
+        #             data={
+        #                 'hits': hit_data
+        #             }
+        #         ),
+        #         PlotConfig(
+        #             plot=particles.particle_track_with_production_vertex,
+        #             data={
+        #                 'hits': hit_filtered,
+        #                 'pairs': pair_filtered,
+        #                 'particles': particle_data,
+        #                 'truth': data['truth']
+        #             },
+        #             args={
+        #                 'line_width': 1.0
+        #             }
+        #         ),
+        #         PlotConfig(
+        #             plot=particles.particle_types,
+        #             data={
+        #                 'hits': hit_filtered,
+        #                 'pairs': pair_filtered,
+        #                 'particles': particle_data,
+        #                 'truth': data['truth']
+        #             }
+        #         )
 
-            ]
-        }).plot(save=save/'particles.pdf')
+        #     ]
+        # }).plot(save=save/'particles.pdf')
 
 
